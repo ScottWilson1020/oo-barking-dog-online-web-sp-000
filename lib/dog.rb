1e3e417f03a5f6f621e3df13c3f091f39814b562
Scott Wilson <scottwilson1020@gmail.com>
2020-08-14 17:22:41 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,6 +2,17 @@
 
 class Dog 
   
-  def
+   def name=(dogs_name)
+    @this_dogs_name = dogs_name
+  end
+ 
+  def name
+    @this_dogs_name
+  end
+  
+  def bark 
+    puts "woof!"
+    
+  end 
 
 end 
